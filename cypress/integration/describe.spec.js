0178d69571c3d0b5184c0b30985ca2f5a22fb359
Leandro Pereira <leandro.nares@gmail.com>
2019-11-30 19:06:53 -0300

    6. Assertivas

--- cypress/integration/describe.spec.js
+++ cypress/integration/describe.spec.js
@@ -3,7 +3,7 @@ it.only('A external test...',()=>{
 
 })
 //GRUPO DE TESTES
-describe.skip('Should group testes...',()=>{
+describe('Should group testes...',()=>{
     describe('Should group more specific tests...',()=>{
         it('A specific test...',() => {
         
