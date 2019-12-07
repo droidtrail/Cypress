89434b29ba0174e74646c4b4eeb158ffa9137507
Leandro Pereira <leandro.nares@gmail.com>
2019-12-07 05:21:14 -0300

    19. Retentativas

--- cypress/integration/Sync.spec.js
+++ cypress/integration/Sync.spec.js
@@ -10,13 +10,20 @@ describe('Esperas...',()=>{
     })
 
     it('Deve aguardar elemento estar disponível',()=>{
-
         cy.get('#novoCampo').should('not.exist')
         cy.get('#buttonDelay').click()
         cy.get('#novoCampo').should('not.exist')
         cy.get('#novoCampo').should('exist')
         cy.get('#novoCampo').type('funciona')
-        
+    })
+
+    it.only('Deve fazer retentativas',()=>{
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo')
+            .should('exist')
+            .type('funciona')
     })
 })
 
