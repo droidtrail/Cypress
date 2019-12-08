58f64d7e3fa4dc524e195c73a8395d6d8e9aaa81
Leandro Pereira <leandro.nares@gmail.com>
2019-12-08 19:58:25 -0300

    23. Sincronizando o título

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -7,17 +7,21 @@ describe('Cypress basics',()=>{
         //const title = cy.title()
         //console.log(title)
 
-        //cy.title().should('be.equal','Campo de Treinamento')
-        //cy.title().should('contain','Campo')
+        cy.title().should('be.equal','Campo de Treinamento')
+        cy.title().should('contain','Campo')
 
-        //cy.title()
-            //.should('be.equal','Campo de Treinamento')
-            //.should('contain','Campo')
+        cy.title()
+            .should('be.equal','Campo de Treinamento')
+            .and('contain','Campo')
 
-         cy.pause()
-         cy.title()
-             .should('be.equal','Campo de Treinamento')
-             .and('contain','Campo')
+        cy.title().should(title => {
+            console.log(title)
+        })
+
+         //cy.pause()
+        // cy.title()
+             //.should('be.equal','Campo de Treinamento')
+             //.and('contain','Campo')
 
              //TODO Imprimir o log no console
              //TODO Escrever o title em um campo de texto
@@ -25,10 +29,10 @@ describe('Cypress basics',()=>{
     })
 
     it('Should find and interact with an element',()=>{
-    cy.visit('https://wcaquino.me/cypress/componentes.html')
-    cy.get('#buttonSimple')
-        .click()
-        .should('have.value','Obrigado!')
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value','Obrigado!')
     })
 
 })
