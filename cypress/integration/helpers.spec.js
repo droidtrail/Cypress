bba27358c10e7428491c09e26477390fad478849
Leandro Pereira <leandro.nares@gmail.com>
2019-12-15 12:20:25 -0300

    27. Invoke

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -43,4 +43,19 @@ describe('Helpers...',()=>{
             cy.title().its('length').should('be.equal',20)
 
         })
+
+        it.only('Invoke...',() =>{
+            const getValue = () => 1;
+            const soma = (a, b) => a + b;
+
+            cy.wrap({fn:getValue}).invoke('fn').should('be.equal',1)
+            cy.wrap({fn:soma}).invoke('fn', 2, 5).should('be.equal',7)
+        
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+            cy.get('#formNome').invoke('val','Texto via invoke')
+            cy.window().invoke('alert','Dá pra ver?')
+            cy.get('#resultado')
+                .invoke('html','<input type = "button", value="Hacked"/>')
+        
+        })
     })
