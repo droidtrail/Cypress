07983c8de7e4f4818b9e9e3847e586592d46effe
Leandro Pereira <leandro.nares@gmail.com>
2020-01-19 20:21:25 -0300

    38. Popup

---
+++ cypress/integration/popup.spec.js
@@ -0,0 +1,36 @@
+// <reference types = "cypress" />
+
+describe('Work with Popup',()=>{
+
+    before(()=>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+    
+    beforeEach(()=>{
+        cy.reload()
+    })
+
+    it('Deve testar frame diretamente', ()=>{
+
+        cy.visit('https://wcaquino.me/cypress/frame.html')
+        
+        cy.get('#otherButton').click()
+        cy.on('window:alert', msg =>{
+            expect(msg).to.be.equal('Click OK!')
+        })
+
+    })
+
+    it.only('Deve verificar se o popup foi invocado', ()=>{
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        
+        cy.window().then(win => {
+            cy.stub(win, 'open').as('winOpen')
+        })
+        cy.get('#buttonPopUp').click()
+        cy.get('@winOpen').should('be.called')
+        
+        
+        })  
+    })
