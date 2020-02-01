77f4ae0355f898accfde3cde30e4fb489bfc2ddc
Leandro Pereira <leandro.nares@gmail.com>
2020-02-01 16:38:55 -0300

    41. Locators

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -10,10 +10,13 @@ describe('Work with basic elements',()=>{
         cy.reload()
     })
 
-it('...',()=>{
+    it('using jquery selector',() => {
 
+        cy.get('table#tabelaUsuarios tbody > tr:eq(0) td:nth-child(3)>input')
+        cy.get("[onclick*='Francisco']")
+        cy.get('table#tabelaUsuarios td:contains(\'Doutorado\'):eq(0) ~ td:eq(3) > input')
+        cy.get('table#tabelaUsuarios tr:contains(\'Doutorado\'):eq(0) td:eq(6) input')
 
-    
-})
+    })
 
 })
