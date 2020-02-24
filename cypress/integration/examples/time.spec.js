7ceb587d671868ccf024afdb5617cfbec21f1c5e
Leandro Pereira <leandro.nares@gmail.com>
2020-02-24 16:08:01 -0300

    49. Tick (Controlando o Tempo)

--- cypress/integration/examples/time.spec.js
+++ cypress/integration/examples/time.spec.js
@@ -21,4 +21,29 @@ describe('Dinamic tests',()=>{
         cy.get('#resultado > span').should('contain','10/04/2012')
 
     })
+
+    it.only('Goes to the future',()=>{
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain','15825')
+        //gt=maior que
+        cy.get('#resultado > span').invoke('text').should('gt',1582569084875)
+        
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        //lte=menor que
+        cy.get('#resultado > span').invoke('text').should('lte',0)
+
+        // cy.wait(1000)
+        // cy.get('#buttonTimePassed').click()
+        // //gt=maior ou igual a...
+        // cy.get('#resultado > span').invoke('text').should('lte',1000)
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte',5000)
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte',15000)
+
+    })
 })
