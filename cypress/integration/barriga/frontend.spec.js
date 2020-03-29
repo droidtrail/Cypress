2b9a5b317d7650f589c0f870b0fbd8b6c6f1e022
Leandro Pereira <leandro.nares@gmail.com>
2020-03-28 22:39:50 -0300

    76. Inserir conta

--- cypress/integration/barriga/frontend.spec.js
+++ cypress/integration/barriga/frontend.spec.js
@@ -17,29 +17,29 @@ describe('Deve fazer teste a nível funcional',()=>{
             token:'Uma string muito grande que não deveria ser aceita, mas vai'
            }
        }).as('Login')
-       //Rota de saldo
+       
        cy.route({
            method:'GET',
            url:'/saldo',
            response:[{
                 conta_id:'999',
-                conta:'Leandro',
+                conta:'Carteira',
                 saldo:'1500.00'
            },
            {
                 conta_id:'9909',
-                conta:'Daniele',
+                conta:'Banco',
                 saldo:'1000000.00'
            },
            ]
        }).as('Saldo')
        cy.login('leandro.nares@gmail.com','senhaErrada')
-       cy.resetApp()
+       //cy.resetApp()
     })
 
     beforeEach(()=>{
         cy.get(loc.MENU.HOME).click()
-        cy.resetApp()
+        // cy.resetApp()
     })
 
     after(()=>{
@@ -47,12 +47,40 @@ describe('Deve fazer teste a nível funcional',()=>{
     })
 
     it('Deve criar uma conta',()=>{
+        cy.route({
+            method:'GET',
+            url:'/contas',
+            response:[
+                {id:'03',nome:'Carteira',visivel:true,usuario_id:1},
+                {id:'04',nome:'Banco',visivel:true,usuario_id:1},
+            ]
+        }).as('Obter Contas')
+
+        cy.route({
+            method:'POST',
+            url:'/contas',
+            response:
+                {id:'05',nome:'Conta de teste',visivel:true,usuario_id:1}
+            
+        }).as('Inserir Conta')
+
         cy.acessarMenuConta()
+
+        cy.route({
+            method:'GET',
+            url:'/contas',
+            response:[
+                {id:'03',nome:'Carteira',visivel:true,usuario_id:1},
+                {id:'04',nome:'Banco',visivel:true,usuario_id:1},
+                {id:'05',nome:'Conta de teste',visivel:true,usuario_id:1}
+            ]
+        }).as('Obter Conta Inserida')
+
         cy.inserirConta('Conta de teste')
         cy.get(loc.MESSAGE).should('contain','Conta inserida com sucesso!')
     })
 
-    it('Deve alterar conta',()=>{
+    it.skip('Deve alterar conta',()=>{
         cy.acessarMenuConta()
         cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click()
         cy.get(loc.CONTAS.NOME)
@@ -62,14 +90,14 @@ describe('Deve fazer teste a nível funcional',()=>{
         cy.get(loc.MESSAGE).should('contain','Conta inserida com sucesso!')   
     })
 
-    it('Não deve criar uma conta com o mesmo nome',()=>{
+    it.skip('Não deve criar uma conta com o mesmo nome',()=>{
         cy.acessarMenuConta()
         cy.inserirConta('Conta mesmo nome')
         cy.get(loc.CONTAS.BTN_SALVAR).click({force:true})
         cy.get(loc.MESSAGE).should('contain','status code 400')
     })
 
-    it('Deve criar uma transação',()=>{
+    it.skip('Deve criar uma transação',()=>{
         cy.get(loc.MENU.MOVIMENTACAO).click()
         cy.get(loc.MOVIMENTACAO.DESCRIÇÃO).type('Desc')
         cy.get(loc.MOVIMENTACAO.VALOR).type('123')
@@ -82,7 +110,7 @@ describe('Deve fazer teste a nível funcional',()=>{
         cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc','123')).should('exist')
     })
 
-    it('Deve pegar o saldo',()=>{
+    it.skip('Deve pegar o saldo',()=>{
         cy.get(loc.MENU.HOME).click()
         cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo','534'))
         cy.get(loc.MENU.EXTRATO).click()
@@ -97,7 +125,7 @@ describe('Deve fazer teste a nível funcional',()=>{
         cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo','4.034,00'))
     })
 
-    it('Deve remover uma movimentação',()=>{
+    it.skip('Deve remover uma movimentação',()=>{
         cy.get(loc.MENU.EXTRATO).click()
         cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
         cy.get(loc.MESSAGE).should('contain','Movimentação inserida com sucesso!')
